ebe5dbcb8eaa9e80de4ead4623821e6659d678d5
meganroberta80 <meganroberta89@gmail.com>
2021-07-22 16:05:47 -0700

    Created a game.html file and split the index.html file into it.

--- js/app.js
+++ js/app.js
@@ -2,17 +2,9 @@
 // Create a Class (JS Class, look at your notes if your forget) for your tamagotchi
 // Instantiate your Tamagotchi
 // Display a character of your choice on the screen to represent your pet
-// Display the following metrics for your pet:
-// Hunger (1-10 scale)
-// Sleepiness (1-10 scale)
-// Boredom (1-10 scale)
 // Age
-// Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
 // Add the ability to name your pet.
 // Style the page.
-// Increase your pet's age every x minutes
-// Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
-// You pet should die if Hunger, Boredom, or Sleepiness hits 10.
 
 // hunger
 let hungerMeter = document.getElementById('hunger-meter')
@@ -30,7 +22,6 @@ let playEl = document.getElementById('btn-play')
 let notifications = document.getElementById('notifications')
 
 let count = 0
-
 
 class Tamagotchi {
     constructor(name, hunger, sleepiness, boredom, age) {
